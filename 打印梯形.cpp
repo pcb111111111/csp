f5ee075177931e1a129dfdceffdf1bc08208dbc2
pcb111111111 <39986296+pcb111111111@users.noreply.github.com>
2021-03-22 15:46:42 +0800

    Update 打印梯形.cpp
    输入：5
    输出：
               *****
            *******
          *********
       ***********
    *************

--- 打印梯形.cpp
+++ 打印梯形.cpp
@@ -1,1 +1,26 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+#define _for(a,b,i) for(int i=a;i<b;i++)
+
+int main(){
+    int h,a;
+    scanf("%d",&h);
+    a=2*(h-1);
+    _for(0,h,i){
+        _for(0,2*(h-1)+h,j){
+            if(j<a)
+                cout<<' ';
+            else
+                cout<<'*';
+
+        }
+        a-=2;
+        cout<<endl;
+    }
+    return 0;
+}
